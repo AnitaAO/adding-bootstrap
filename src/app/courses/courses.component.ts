f81d3dba8167285f6dc023ebc6d4075543d930dc
Anita <anitaisbeta@gmail.com>
2022-08-01 18:42:15 +0100

    update app component

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -2,10 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
-  templateUrl: './courses.component.html',
+  template: `
+    <h1>{{ title }}</h1>
+    <button class="btn btn-primary"></button>
+  `,
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent implements OnInit {
+export class CoursesComponent {
+  title = "Bible";
 
   constructor() { }
 
